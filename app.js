ae43e75bda0320e474422bc91b5bb4ba1b315aac
Saurabh Rawat <119805882+learnersaura-bh@users.noreply.github.com>
2022-12-21 15:52:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -49,7 +49,9 @@ export default function App() {
 
       <ol style={{textAlign: "center"}}>
         {podcasts[genreSelected].map((key) => (
-          <li style={{ textAlign: "center" }}>{key.name} / rating = {key.rating}</li>
+           <div> {key.name} </div>
+            <div style={{ fontSize: "small" }}> rating : {key.rating} </div>
+            <hr />
         ))}
       </ol>
     </div>
